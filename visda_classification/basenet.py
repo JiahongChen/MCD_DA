41ae05dd4d672425b89ee983dd006194ec685476
Peter <15019313+JiahongChen@users.noreply.github.com>
2019-08-03 12:07:37 -0700

    debug

--- visda_classification/basenet.py
+++ visda_classification/basenet.py
@@ -109,7 +109,7 @@ class AlexNet2(nn.Module):
     def __init__(self):
         super(AlexNet2, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.Conv2d(3, 64, kernel_size=5, stride=4, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
