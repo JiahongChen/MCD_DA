0cb8b227685221a8823b47573948d2bbea3c1988
Peter <15019313+JiahongChen@users.noreply.github.com>
2019-08-05 15:37:06 -0700

    modified:   visda_classification/res_train_main.py

--- visda_classification/res_train_main.py
+++ visda_classification/res_train_main.py
@@ -19,8 +19,8 @@ parser.add_argument('--batch-size', type=int, default=32, metavar='N',
 					help='input batch size for training (default: 32)')
 parser.add_argument('--test-batch-size', type=int, default=32, metavar='N',
 					help='input batch size for testing (default: 32)')
-parser.add_argument('--epochs', type=int, default=50, metavar='N',
-					help='number of epochs to train (default: 50)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+					help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
 					help='learning rate (default: 0.001)')
 parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
